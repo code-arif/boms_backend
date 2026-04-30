b79cc8897cc2cfdc4a3c97b96252efd12acb7860
code-arif <arifulislam6460@gmail.com>
2026-04-30 14:10:05 +0600

    Add AssignAdminRequest for validating admin assignment requests

---
+++ app/Http/Requests/AssignAdminRequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AssignAdminRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'user_id' => ['required', 'integer', 'exists:users,id'],
+        ];
+    }
+}
